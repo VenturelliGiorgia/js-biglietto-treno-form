f114e11ce3173748f597bf79360333b732b4d261
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2022-10-29 23:55:48 +0200

    select funzionante

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const km = parseInt(prompt("Quanti km vuoi percorrere?"));
+const costoKm = .21;
+const costoKmTotal = km * .21;
+const fullName = prompt("Come ti chiami?");
+
+const inputEl = document.querySelector("[name='fullname']");
+console.log(inputEl);
+inputEl.value = fullName;
+console.log(inputEl.value);
+
+const inputKmEl = document.querySelector("[name='km']");
+console.log(inputEl);
+inputKmEl.value = km;
+console.log(inputKmEl.value);
+
+
+const btnGenera = document.getElementById("btn-genera");
+console.log(costoKmTotal);
+let value;
+const mySelect = document.getElementById("mySelect");
+btnGenera.addEventListener("click", function myFunction() {
+
+    const indiceSelect = mySelect.selectedIndex; //variabile che gestisce l'indice della select con le suo option
+    const valoreSelect = mySelect.options[indiceSelect]; //le associa le opzioni 
+    const valoreOpzione = valoreSelect.value; //da un valore alle opzioni 
+    console.log(valoreSelect);
+
+    if (valoreOpzione == 20) {
+        const costoTotal = costoKmTotal * .80;
+        let num = costoTotal.toFixed(2);
+        paragrafo.innerHTML = `Costo biglietto, con l'aggiunta del 20% di sconto: <strong> ${num}Є</strong>`;
+    } else if (valoreOpzione == 40) {
+        const costoTotal = costoKmTotal * .60;
+        let num = costoTotal.toFixed(2);
+        paragrafo.innerHTML = `Costo biglietto, con l'aggiunta del 40% di sconto: <strong>${num}Є</strong>`
+    } else {
+        let num = costoKmTotal.toFixed(2);
+        paragrafo.innerHTML = `Costo biglietto: <strong>${num}Є</strong>`
+    }
+
+    console.log(mySelect.value);
+});
+
+
